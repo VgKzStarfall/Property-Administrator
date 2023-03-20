73fded5853524ae2d9bf101e5329badae42225c0
Vanka Giucop <81300814+VgKzStarfall@users.noreply.github.com>
2023-03-20 22:39:22 +0700

    Fix BO - XstoreContext

---
+++ BusinessObject/LandlordObject.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessObject
+{
+    public class LandlordObject
+    {
+        public int LandlordId { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Location { get; set; }
+        public string CitizenId { get; set; }
+    }
+}
--- BusinessObject/PropertyObject.cs
+++ BusinessObject/PropertyObject.cs
@@ -9,9 +9,10 @@ namespace BusinessObject
     public class PropertyObject
     {
         public int PropertyId { get; set; }
-        public string pName { get; set; }
-        public string pLocation { get; set; }
-        public float pArea { get; set; }
-        public int pPrice { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double Area { get; set; }
+        public decimal Price { get; set; }
+        public string Available { get; set; }
     }
 }
